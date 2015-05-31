e7fc6d41a38c8af0935e1a34a24222fc26d0fd5f
Jonas Eichenberger <jo.eichenberger@gmail.com>
2015-05-31 19:56:18 +0200

    plot pitch data from bag file

--- matlab/plotBagFiles.m
+++ matlab/plotBagFiles.m
@@ -1,32 +1,33 @@
 % SP Jonas
 
-filePath = fullfile('../bagFiles', '2015-05-29-18-31-14.bag');
+filePath = fullfile('../bagFiles', '2015-05-29-18-16-40_pitch.bag');
 bag = rosbag(filePath);
 
 % Get Pitch Data
-% msgs = readMessages(select(bag, 'Topic', '/naro_monitoring/pitchLog'));
-% n = length(msgs);
-% m = length(msgs{1}.Data);
-% 
-% pitchLogData = zeros(n,m);
-% for i = 1:n
-%    pitchLogData(i,:) = msgs{i}.Data; 
-% end
-% 
-% t = pitchLogData(:,end);
-
-
-
-% Get Depth Data
-msgs = readMessages(select(bag, 'Topic', '/naro_monitoring/depthLog'));
+msgs = readMessages(select(bag, 'Topic', '/naro_monitoring/pitchLog'));
 n = length(msgs);
 m = length(msgs{1}.Data);
 
-depthLogData = zeros(n,m);
+pitchLogData = zeros(n,m);
 for i = 1:n
-   depthLogData(i,:) = msgs{i}.Data; 
+   pitchLogData(i,:) = msgs{i}.Data; 
 end
 
-t = depthLogData(:,end);
-figure(2)
-plot(t, depthLogData(:,1:m-1))
+t = pitchLogData(:,end);
+figure(1)
+plot(t, pitchLogData(:,1:m-1)*180/pi)
+
+
+% Get Depth Data
+% msgs = readMessages(select(bag, 'Topic', '/naro_monitoring/depthLog'));
+% n = length(msgs);
+% m = length(msgs{1}.Data);
+% 
+% depthLogData = zeros(n,m);
+% for i = 1:n
+%    depthLogData(i,:) = msgs{i}.Data; 
+% end
+% 
+% t = depthLogData(:,end);
+% figure(2)
+% plot(t, depthLogData(:,1:m-1))
